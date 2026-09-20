2297d61e5664c101c642ebfe54dea48160a19d79
pslohi <pslohi@student.jyu.fi>
2026-09-20 15:05:07 +0300

    implement click movements and highlight selected square

--- frontend/src/components/ChessBoard.jsx
+++ frontend/src/components/ChessBoard.jsx
@@ -4,6 +4,7 @@ import { Chess } from 'chess.js';
 
 export function ChessBoard() {
     const [game, setGame] = useState(() => new Chess());
+    const [selectedSquare, setSelectedSquare] = useState(null);
 
     // Handles piece drop events
     function onPieceDrop({ sourceSquare, targetSquare }) {
@@ -15,6 +16,41 @@ export function ChessBoard() {
 
         return makeMove(sourceSquare, targetSquare); // Try to make the move and return the result.
     }
+
+    // Handles square click events
+    function onSquareClick({ square }) {
+        // No square is selected yet -> select this one
+        if (!selectedSquare) {
+            const piece = game.get(square);
+
+            // Select only pieces with the current turn's color
+            if (piece && piece.color === game.turn()) {
+                setSelectedSquare(square);
+            }
+
+            return;
+        }
+
+        // Clicked the same square -> remove selection
+        if (selectedSquare === square) {
+            setSelectedSquare(null);
+            return;
+        }
+
+        // Try to make a move
+        const moveSuccessful = makeMove(selectedSquare, square);
+
+        // If the move was not successful, check if another piece of the same color was selected
+        if (!moveSuccessful) {
+            const piece = game.get(square);
+
+            if (piece && piece.color === game.turn()) {
+                setSelectedSquare(square);
+            } else {
+                setSelectedSquare(null);
+            }
+        }
+    }
     
     // Attempts to make a move and returns whether it was successful.
     function makeMove(sourceSquare, targetSquare) {
@@ -25,10 +61,11 @@ export function ChessBoard() {
             gameCopy.move({
                 from: sourceSquare,
                 to: targetSquare,
-                promotion: 'q',
+                promotion: 'q', // Always promote to queen TODO: Add UI for promotion choice
             });
 
             setGame(gameCopy);
+            setSelectedSquare(null);
             return true;
 
         } catch {
@@ -42,7 +79,10 @@ export function ChessBoard() {
         <Chessboard
             options={{
                 position: game.fen(), // FEN representing current game state
-                onPieceDrop
+                onPieceDrop, 
+                onSquareClick, 
+                // Highlight the selected square 
+                squareStyles: selectedSquare ? { [selectedSquare]: { background: 'rgba(255, 255, 0, 0.5)' } } : {},
             }}  
         />
     );
