49cb15505dcb629bb8e3dd49d6591e9ab9730d40
pslohi <pslohi@student.jyu.fi>
2026-09-20 13:51:48 +0300

    implement drag-and-drop piece movement

--- frontend/src/components/ChessBoard.jsx
+++ frontend/src/components/ChessBoard.jsx
@@ -1,15 +1,50 @@
+import { useState } from 'react';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
 
 export function ChessBoard() {
+    const [game, setGame] = useState(() => new Chess());
+
+    // Handles piece drop events
+    function onPieceDrop({ sourceSquare, targetSquare }) {
+
+        // If the piece was not dropped on square, the move is not accepted.
+        if (!sourceSquare || !targetSquare) {
+            return false;
+        }
+
+        return makeMove(sourceSquare, targetSquare); // Try to make the move and return the result.
+    }
     
+    // Attempts to make a move and returns whether it was successful.
+    function makeMove(sourceSquare, targetSquare) {
 
+        const gameCopy = new Chess(game.fen()); // Make copy of the current game
+        
+        try {
+            gameCopy.move({
+                from: sourceSquare,
+                to: targetSquare,
+                promotion: 'q',
+            });
 
+            setGame(gameCopy);
+            return true;
 
+        } catch {
+            return false;
+        }
+    }
 
-
+    
     return (
-        <Chessboard/>
+        // Render the chessboard with handlers.
+        <Chessboard
+            options={{
+                position: game.fen(), // FEN representing current game state
+                onPieceDrop
+            }}  
+        />
     );
 };
 
