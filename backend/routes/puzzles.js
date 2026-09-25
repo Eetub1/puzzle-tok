e00aeb4edb0b0a1a1242d2833a650ef24a501da2
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-09-25 14:31:51 +0300

    retrieve by id temp works

--- backend/routes/puzzles.js
+++ backend/routes/puzzles.js
@@ -2,6 +2,34 @@ import express, { response, Router } from "express"
 const puzzlesRouter = express.Router()
 const base_url = `https://lichess.org`
 
+
+const getGameById = async id => {
+
+    console.log("Retrieving game by id")
+
+    let response = await fetch(`${base_url}/api/puzzle/${id}`)
+
+    if (!response.ok) {
+		throw new Error(`Lichess request failed: ${response.status}`)
+	}
+
+    console.log("puzzle", response)
+
+    /*
+    // Need to use a different method if max is a larger number
+    const text = await response.text()
+    // Split result, remove empty lines
+    const game = text
+        .split("\n")
+        .filter(line => line.trim() !== "")
+        .map(line => JSON.parse(line)) // parse line into javascript object
+
+    console.log('game: ', game)*/
+
+    return response.json()
+}
+
+
 puzzlesRouter.get('/daily', async (req,res) => {
 
     console.log("Sending daily puzzle")
@@ -26,25 +54,29 @@ puzzlesRouter.get('/next', async (req,res) => {
     res.send(await response.json())
 })
 
+
+
 puzzlesRouter.get('/puzzlebyid', async (req,res) => {
 
     let id = 'y14HH'
     console.log("Sending next by id")
 
-    let response = await fetch(`${base_url}/api/puzzle/${id}`)
+    if (!id) {
+        return res.status(400).json({ error: "Game with id not found" })
+    }
+    
+    let game = await getGameById(id)
 
-    if (!response.ok) {
-		throw new Error(`Lichess request failed: ${response.status}`)
-	}
+    if (!game) {
+        return res.status(502).json()
+    }
 
-    console.log("puzzle", response)
-
-    res.send(await response.json())
+    res.send(game)
 })
 
 puzzlesRouter.get('/batch', async (req,res) => {
 
-    const query = new URLSearchParams({
+    const query = new URLSearchParams({ //TODO: add possibility to filter
         "difficulty": "normal",
         "nb": 5,
         "color": "white"
